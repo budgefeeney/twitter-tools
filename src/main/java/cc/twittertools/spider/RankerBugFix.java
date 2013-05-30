c25447c1a85715a12e1f06629609c232917f6e15
bryan.feeney <bryan.feeney@gmail.com>
2013-05-30 15:10:06 +0200

    Script to patch the output of the first ranker

---
+++ src/main/java/cc/twittertools/spider/RankerBugFix.java
@@ -0,0 +1,13 @@
+package cc.twittertools.spider;
+
+/**
+ * First run of the ranker wrote out the date in the wrong format,
+ * and wrongly calculated the user age, thereby getting the sort
+ * order wrong.
+ * @author bfeeney
+ *
+ */
+public class RankerBugFix
+{
+  
+}
