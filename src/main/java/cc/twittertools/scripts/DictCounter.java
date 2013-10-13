fdf17e0a8732f2c811d74c8da25af86c027c9643
Bryan Feeney <bryan.feeney@gmail.com>
2013-10-13 19:20:47 +0100

    Do all dictionary counting in Dictcounter

--- src/main/java/cc/twittertools/scripts/DictCounter.java
+++ src/main/java/cc/twittertools/scripts/DictCounter.java
@@ -9,11 +9,11 @@ import cc.twittertools.words.dict.LookupDictionary;
 public class DictCounter
 {
 	public static void main (String[] args) throws IOException
-	{	final int  MIN_OCCURRENCE_COUNT = 10;
+	{	//final int  MIN_OCCURRENCE_COUNT = 10;
 		final Path PATH = Paths.get("/Users/bryanfeeney/Desktop/DatasetStats");
 		
 		for (int minOccurenceCount : new int[] { 1, 5, 10})
-		{	for (String dictFile : new String[] { "dictionary.txt", "hashtags.txt", "addressees.txt", "urls.txt" } )
+		{	for (String dictFile : new String[] { "smileys.txt", "dictionary.txt", "hashtags.txt", "addressees.txt", "urls.txt", "stocks.txt" } )
 			{	LookupDictionary dict = LookupDictionary.fromFile(PATH.resolve(dictFile), minOccurenceCount);
 			
 				System.out.println ("With min-occurrence = " + minOccurenceCount + ", " + dictFile.replaceAll("\\.txt", ".size") + " = " + dict.size());
