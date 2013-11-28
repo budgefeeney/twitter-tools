9cd1f3131a240fe79440a7df6bb4205ccbcebe06
Bryan Feeney <bryan.feeney@gmail.com>
2013-11-28 18:06:45 +0000

    Ignore log files, add a launch script

---
+++ launch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+params="encode"
+params="$params --infile /Users/bryanfeeney/opt/twitter-tools-spider/src/test/resources/spider"
+params="$params --outfile /Users/bryanfeeney/Desktop/SmallDB"
+params="$params --skip-retweets"
+params="$params --start-date 20130501"
+params="$params --end-date 20131001"
+params="$params --enable-stemming"
+params="$params --min-word-len 1"
+params="$params --max-word-len 1000"
+params="$params --min-word-count 10"
+params="$params --incl-numbers"
+params="$params --feat-author"
+params="$params --feat-woy"
+params="$params --feat-moy"
+params="$params --dict-addrs __null_dict"
+params="$params --dict-urls /Users/bryanfeeney/Desktop/DatasetStats/urls.txt"
+params="$params --dict-words /Users/bryanfeeney/Desktop/DatasetStats/dictionary.txt"
+params="$params --dict-stocks /Users/bryanfeeney/Desktop/DatasetStats/stocks.txt"
+params="$params --dict-smileys /Users/bryanfeeney/Desktop/DatasetStats/smileys.txt"
+params="$params --dict-tags /Users/bryanfeeney/Desktop/DatasetStats/hashtags.txt"
+params="$params --selected-acs /Users/bryanfeeney/Dropbox/usershorter.txt"
+
+jar_name="twitter-tools"
+jar_version="1.1.1"
+jar_dir="target"
+jar_file="$jar_dir/$jar_name-$jar_version-jar-with-dependencies.jar"
+
+java -Xmx3000m -cp $jar_file cc.twittertools.scripts.Main $params
