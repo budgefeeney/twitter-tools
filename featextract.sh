fed2d4e4340de8b13dfe77da1d8ce0830d495591
Bryan Feeney <bryan.feeney@gmail.com>
2013-12-13 20:28:13 +0000

    Make feat-extract script location independent
    Also change some parameter values.

--- featextract.sh
+++ featextract.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+SRC_DIR=`dirname $0`
+
 params="encode"
 params="$params --infile /Users/bryanfeeney/opt/twitter-tools-spider/src/test/resources/spider"
-params="$params --outfile /Users/bryanfeeney/Desktop/SmallerDB-20"
+params="$params --outfile /Users/bryanfeeney/Desktop/SmallDB-NoCJK"
 params="$params --skip-retweets"
 params="$params --start-date 20130501"
 params="$params --end-date 20131001"
 params="$params --enable-stemming"
 params="$params --min-word-len 1"
 params="$params --max-word-len 1000"
-params="$params --min-word-count 20"
+params="$params --min-word-count 50"
 params="$params --incl-numbers"
 params="$params --feat-author"
 params="$params --feat-woy"
 params="$params --feat-moy"
 params="$params --dict-addrs __null_dict"
-params="$params --dict-urls /Users/bryanfeeney/Desktop/DatasetStats2/urls-sorted.dict"
-params="$params --dict-words /Users/bryanfeeney/Desktop/DatasetStats2/words-sorted.dict"
-params="$params --dict-stocks /Users/bryanfeeney/Desktop/DatasetStats2/stocks-sorted.dict"
-params="$params --dict-smileys /Users/bryanfeeney/Desktop/DatasetStats2/smileys-sorted.dict"
-params="$params --dict-tags /Users/bryanfeeney/Desktop/DatasetStats2/hashtags-sorted.dict"
+params="$params --dict-urls /Users/bryanfeeney/Desktop/SmallDB-NoCJK/Stats/urls-ordered.dict"
+params="$params --dict-words /Users/bryanfeeney/Desktop/SmallDB-NoCJK/Stats/words-ordered.dict"
+params="$params --dict-stocks /Users/bryanfeeney/Desktop/SmallDB-NoCJK/Stats/stocks-ordered.dict"
+params="$params --dict-smileys /Users/bryanfeeney/Desktop/SmallDB-NoCJK/Stats/smileys-ordered.dict"
+params="$params --dict-tags /Users/bryanfeeney/Desktop/SmallDB-NoCJK/Stats/hashtags-ordered.dict"
 params="$params --selected-acs /Users/bryanfeeney/Dropbox/SideTopicDatasets/user-even-more-shorter.txt"
 
 jar_name="twitter-tools"
 jar_version="1.1.1"
-jar_dir="target"
+jar_dir="$SRC_DIR/target"
 jar_file="$jar_dir/$jar_name-$jar_version-jar-with-dependencies.jar"
 
 echo $params
