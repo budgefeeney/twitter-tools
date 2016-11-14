89f78790394b9e02297d722ebfdc48c248dcfddc
Bryan Feeney <bryan.feeney@gmail.com>
2016-11-14 13:04:34 +0000

    Increase connection etc. timeouts to 15sec
    Just in case the Internet connection or twitter.com is a bit slow

--- src/main/java/cc/twittertools/download/AsyncEmbeddedJsonStatusBlockCrawler.java
+++ src/main/java/cc/twittertools/download/AsyncEmbeddedJsonStatusBlockCrawler.java
@@ -49,9 +49,9 @@ public class AsyncEmbeddedJsonStatusBlockCrawler {
   private static final int TWEET_BLOCK_SIZE = 500;
   
   public static final int MAX_CONNECTIONS = 100;
-  public static final int CONNECTION_TIMEOUT = 10000;
-  public static final int IDLE_CONNECTION_TIMEOUT = 10000;
-  public static final int REQUEST_TIMEOUT = 10000;
+  public static final int CONNECTION_TIMEOUT = 15000;
+  public static final int IDLE_CONNECTION_TIMEOUT = 15000;
+  public static final int REQUEST_TIMEOUT = 15000;
   public static final int MAX_RETRY_ATTEMPTS = 2;
   public static final int WAIT_BEFORE_RETRY = 1000;
   private static final Timer timer = new Timer(true);
